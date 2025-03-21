256590cdcb805f9767a6bdffa3b2c91ebf9836b4
Mrowr Purr <mrowr.purr@gmail.com>
2025-03-20 19:52:19 -0700

    Hello, world :P

--- plugin.cpp
+++ plugin.cpp
@@ -1,7 +1,6 @@
 #include <SkyrimScripting/Plugin.h>
-#include <_Log_.h>
 
-_OnDataLoaded_ {
-    PrintToConsole("EXAMPLE: On Data Loaded!");
-    _Log_("EXAMPLE: On Data Loaded!");
+SKSEPlugin_OnDataLoaded {
+    ConsoleLog("Hello, world!");
+    Log("Hello, world!");
 }
--- xmake.lua
+++ xmake.lua
@@ -2,29 +2,38 @@ add_rules("mode.debug", "mode.release")
 
 set_languages("c++23")
 
--- Required to use PrintToConsole()
-add_cxxflags("/Zc:preprocessor")
+-- Optional, required to use PrintToConsole() macro
+-- add_cxxflags("/Zc:preprocessor")
 
 add_repositories("SkyrimScripting https://github.com/SkyrimScripting/Packages.git")
 add_repositories("SkyrimScriptingBeta https://github.com/SkyrimScriptingBeta/Packages.git")
 add_repositories("MrowrLib https://github.com/MrowrLib/Packages.git")
 
-add_requires("SkyrimScripting.Plugin", { configs = {
-    commonlib = "skyrim-commonlib-ae",
-    use_log_library = true,
-    use_skyrimscripting_logging = true,
-    use_skse_plugin_info_library = true
-}})
+commonlib_version = "ng"
 
-mod_info = {
-    name = "Example of SkyrimScripting Plugin",
-    version = "0.0.1",
-    author = "Mrowr Purr",
-    email = "mrowr.purr@gmail.com",
-    mod_files = {"Scripts"}
-}
+add_requires("skyrim-commonlib-" .. commonlib_version)
+add_requires("SkyrimScripting.Plugin", { configs = { commonlib = "skyrim-commonlib-" .. commonlib_version } })
 
-skyrim_versions = {"ng"}
+mod_info = { name = "Example of SkyrimScripting Plugin" }
 
-includes("papyrus.lua")
-includes("skse.lua")
+mods_folders = os.getenv("SKYRIM_MODS_FOLDERS")
+
+if mods_folders then
+    mod_info.mods_folders = mods_folders
+else
+    print("SKYRIM_MODS_FOLDERS environment variable not set")
+end
+
+target("SKSE Plugin - " .. commonlib_version:upper())
+    set_basename(mod_info.name .. "-" .. commonlib_version:upper())
+    add_files("*.cpp")
+    add_rules("@skyrim-commonlib-" .. commonlib_version .. "/plugin", {
+        mod_name = mod_info.name .. " (" .. commonlib_version .. ")",
+        mods_folders = mod_info.mods_folders or "",
+        mod_files = mod_info.mod_files,
+        name = mod_info.name,
+        version = mod_info.version,
+        author = mod_info.author,
+        email = mod_info.email
+    })
+    add_packages("SkyrimScripting.Plugin")
